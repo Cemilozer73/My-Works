66947790b943137f45aeba524965b46ce48e3dd5
CemilOzer <cemilozer73@gmail.com>
2023-08-10 11:03:26 +0300

    Myworks

--- src/August10_MyWork/C01.java
+++ src/August10_MyWork/C01.java
@@ -2,6 +2,7 @@ package August10_MyWork;
 
 public class C01 {
     public static void main(String[] args) {
-        System.out.println("Yeniden Merhaba");
+        System.out.println("Yeniden Merhaba dedim");
+
     }
 }

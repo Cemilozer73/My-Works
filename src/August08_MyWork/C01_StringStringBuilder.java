920727643e9918cf43da41929b30d5de38c628c2
CemilOzer <cemilozer73@gmail.com>
2023-08-11 14:15:17 +0300

    My Work

--- src/August08_MyWork/C01_StringStringBuilder.java
+++ src/August08_MyWork/C01_StringStringBuilder.java
@@ -9,11 +9,14 @@ public class C01_StringStringBuilder {
         System.out.println(str.toUpperCase());  // ALİ : Methodun buyuk harfe çevrilmesi kalici olmaz.
 
         System.out.println(str.substring(1,2)); // l : Yine atama yapilmadigi icin degisiklik kalici olmaz.
+
         System.out.println(str); // Ali :String immutable'dır atama yapilmaz ise ilk halini korur, değişmez.
-        str =  "Ali Can";
+
+        str =  "Ali Kemal Can"; // Atama yapmis olduk
         System.out.println(str);
+
         System.out.println("------------------------------------");
-        //---------------------------------------------------------------------------------------------------//
+        System.out.println("------------------------------------");
 
         StringBuilder sb1 = new StringBuilder("Ali"); // Mutable oldugu icin StringBuilder'de yapilan her
                                                      // degisiklik kalici olacaktir.
--- src/August08_MyWork/C02_StringBuilder.java
+++ src/August08_MyWork/C02_StringBuilder.java
@@ -1,33 +1,46 @@
 package August08_MyWork;
 
-public class C02_StringBuilder {
+public class  C02_StringBuilder {
 
-    /* Soru: Asagidaki kod blogu calistiginda, konsolda hangi sonuçlar cikar?
-
-    A. s1 = a
-    B. s1 = s1
-    C. s2 = s2
-    D. s2 = s2b
-    E. s3 = a
-    F. s3 = null
-    G. Kod calismaz. */
+    /* Soru: Asagidaki kod blogu calistiginda, konsolda hangi sonuçlar cikar? */
 
 
-    public static StringBuilder work(StringBuilder a,
-                                               StringBuilder b) {
+    // class adiyla ayni degil, method
+    // StringBuilder döndüren work methodu
+    // public static yanindaki StringBuilder return type'ı
+    // StringBuilder a, StringBuilder b olmak üzere iki tane StringBuilder parametre alıyor
+    // return a; olarak bir tane StringBuilder döndürüyor.
+
+    public static StringBuilder work(StringBuilder a  ,StringBuilder b) {// 5. olarak method devrede
         a = new StringBuilder("a");
-        b.append("b");
-        return a;
+        b.append("b"); // string builder'in elementlerine bir ekleme yapiyor.
+        return a; // a degeriyle birlikte return ediliyor ve s3="a" oluyor, atama yapmis oluyoruz.
     }
 
-    public static void main(String[] args) {
-        StringBuilder s1 = new StringBuilder("s1");
-        StringBuilder s2 = new StringBuilder("s2");
-        StringBuilder s3 = work(s1, s2);
-        System.out.println("s1 = " + s1);
-        System.out.println("s2 = " + s2);
-        System.out.println("s3 = " + s3);
+    public static void main(String[] args) { // 1. ilk olarak main method calisir.
+
+        StringBuilder s1 = new StringBuilder("s1"); // 2. s1 isminde stringbuilder obje olusturulacak degeri de yine s1
+
+        StringBuilder s2 = new StringBuilder("s2"); // 3. s2 isminde stringbuilder obje olusturulacak degeri de yine s2
+
+        StringBuilder s3 = work(s1, s2); //4.  s3 objesi de bir islem sonucunda olusturuluyor yani work methoduna gidiyor.
+                                         //work(s1, s2) karsiligi method'taki work(StringBuilder a  ,StringBuilder b)'nin
+                                         // karsiligidir. s1 ve s2 StringBuilderinin degerleri gitmis oldu. "s1" ve "s2"
+
+        System.out.println("s1 = " + s1); // s1 = s1
+
+        System.out.println("s2 = " + s2); // s2 = s2b
+
+        System.out.println("s3 = " + s3); // s3 = a
     }
 }
 
 
+   /*
+    A. s1 = a
+B. s1 = s1
+    C. s2 = s2
+D. s2 = s2b
+E. s3 = a
+    F. s3 = null
+    G. Kod calismaz. */

e0fea57a25dfa6e6d07e965f83244fbbc7d38172
CemilOzer <cemilozer73@gmail.com>
2023-08-22 01:02:34 +0300

    22 august

--- src/august22_MyWorks/Soru_2.java
+++ src/august22_MyWorks/Soru_2.java
@@ -5,9 +5,9 @@ public class Soru_2 {
 
         // Soru: Asagidaki Java program parçacığı çalıştırıldıktan sonra konsolda ekran çıktısı ne olur?
 
-        int [] sayilar = {5, -9, 8, 1, 0, -3};
+        int [] arr = {5, -9, 8, 1, 0, -3};
         int toplam = 0;
-        for (int i : sayilar) {
+        for (int i : arr) {
             toplam += i; }
         System.out.println(toplam);
     }
